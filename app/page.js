a92c8a0b46de5067eef1a55b8e2e968c9f2e4232
Crispim Dos Reis Rodrigues <crispim.rodrigues.oficial@gmail.com>
2024-03-23 16:26:07 -0300

    config v2

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
+import Study from "./components/Section.why-study";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <p>Ola mundo</p>
-        <p>Ola mundo</p>
+        <Study/>
     </main>
   );
 }
