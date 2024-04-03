b244c8f769910e32d693ba379391e7121669039e
Crispim Dos Reis Rodrigues <crispim.rodrigues.oficial@gmail.com>
2024-04-02 23:00:48 -0300

    Add Study

--- app/components/Section.why-study.js
+++ app/components/Section.why-study.js
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import Container from './Container'
+import Image from 'next/image'
+import styles from './Study.module.css'
+import Mapa from '@/public/Mapa.png'
 export default function Study() {
   return (
-    <div>Study</div>
+    <section className={styles.study}>
+      <h1 className={styles.title}>Por que estudar na FMU?</h1>
+      <Container>
+        <div className={styles.box}>
+          <div className={styles.subtitle}>
+            <h3>A <span style={{ color: "#FF3C45" }}>FMU</span> está presente em mais<br/> de <span style={{ color: "#FF3C45" }}>300</span> unidades em todas as<br/> regiões do Brasil.</h3>
+            <a href="" className={styles.link}>Saiba mais</a>
+          </div>
+          <div className={styles.map}>
+            <Image src={Mapa} alt='' width={400} height={"auto"} />
+            <span className={styles.desc}>+ de 100<br/>cursos de <br/>Graduação</span>
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
