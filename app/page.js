d8904695e659a0202bf04ed0e80426f4ed7f1840
Churros <klebersonluiz5@gmail.com>
2024-05-13 23:34:15 -0300

    Alterando tudo, mas ainda falta muito ajuste, ajustando o netlify

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ import Free from "./components/Section.free";
 // import Study from "./components/Section.why-study";
 import Location from "./components/Section.location";
 import SectionNotFind from "./components/SectionNotFind";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 
 export default function Page() {
